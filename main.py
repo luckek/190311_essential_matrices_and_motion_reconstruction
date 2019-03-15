0296b745a51a44689f172baf195ee5d3cc4b52c9
Kyle Lucke <luckek1354@gmail.com>
2019-03-15 16:03:32 -0600

    Motion reconstruction working

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import sys
 import matplotlib.pyplot as plt
 from SIFTWrapper import *
 from PIL import Image, ExifTags
-from InverseCamera import *
-from Camera import *
-
 import numpy as np
+
+# Need this to plot 3d
 from mpl_toolkits.mplot3d import axes3d
 
 
-def calc_X0(cam_list):
+def triangulate(P0, P1, x1, x2):
 
-    X0 = np.zeros((3))
+    # P0,P1: projection matrices for each of two cameras/images
+    # x1,x1: corresponding points in each of two images (If using P that has been scaled by K, then use camera
+    # coordinates, otherwise use generalized coordinates)
+    A = np.array([[P0[2, 0] * x1[0] - P0[0, 0], P0[2, 1] * x1[0] - P0[0, 1], P0[2, 2] * x1[0] - P0[0, 2], P0[2, 3] * x1[0] - P0[0, 3]],
+                  [P0[2, 0] * x1[1] - P0[1, 0], P0[2, 1] * x1[1] - P0[1, 1], P0[2, 2] * x1[1] - P0[1, 2], P0[2, 3] * x1[1] - P0[1, 3]],
+                  [P1[2, 0] * x2[0] - P1[0, 0], P1[2, 1] * x2[0] - P1[0, 1], P1[2, 2] * x2[0] - P1[0, 2], P1[2, 3] * x2[0] - P1[0, 3]],
+                  [P1[2, 0] * x2[1] - P1[1, 0], P1[2, 1] * x2[1] - P1[1, 1], P1[2, 2] * x2[1] - P1[1, 2], P1[2, 3] * x2[1] - P1[1, 3]]])
 
-    # Use the average position of the cameras to compute initial guess
-    for cam in cam_list:
-        X0 += cam.cam_mtx[:, 3].T
+    u, s, vt = np.linalg.svd(A)
 
-    X0 /= len(cam_list)
-    return X0
+    return vt[-1]
 
 
 def intrinsic_cam_mtx(f, cu, cv):
@@ -68,15 +70,16 @@ def get_inliers(u1, u2, K):
     u1 = np.column_stack((u1, np.ones(shape=(u1.shape[0], 1))))
     u2 = np.column_stack((u2, np.ones(shape=(u2.shape[0], 1))))
 
-    # plot_matches(im_1, u1, im_2, u2)
-
     K_inv = np.linalg.inv(K)
 
     x1 = u1 @ K_inv.T
     x2 = u2 @ K_inv.T
 
     E, inliers = cv2.findEssentialMat(x1[:, :2], x2[:, :2], np.eye(3), method=cv2.RANSAC, threshold=1e-3)
+
+    # Flatten inlier mask for use w/ numy arrays
     inliers = inliers.ravel().astype(bool)
+
     n_in, R, t, _ = cv2.recoverPose(E, x1[inliers, :2], x2[inliers, :2], cameraMatrix=K)
 
     # Can use this to plot inliers
@@ -89,21 +92,18 @@ def get_inliers(u1, u2, K):
     # I_new[:, :w, :] = im_1
     # I_new[:, w:, :] = im_2
     # plt.imshow(I_new)
-    # flat_inliers = inliers.ravel().astype(bool)
-    # plt.scatter(u1[flat_inliers, 0][::skip], u1[flat_inliers, 1][::skip])
-    # plt.scatter(u2[flat_inliers, 0][::skip] + w, u2[flat_inliers, 1][::skip])
-    # [plt.plot([u1[0], u2[0] + w], [u1[1], u2[1]]) for u1, u2 in zip(u1[flat_inliers][::skip], u2[flat_inliers][::skip])]
+    # plt.scatter(u1[inliers, 0][::skip], u1[inliers, 1][::skip])
+    # plt.scatter(u2[inliers, 0][::skip] + w, u2[inliers, 1][::skip])
+    # [plt.plot([u1[0], u2[0] + w], [u1[1], u2[1]]) for u1, u2 in zip(u1[inliers][::skip], u2[inliers][::skip])]
     # plt.show()
 
-    # FIXME:
-    # return np.hstack((R, t)), u1[inliers, :2], u2[inliers, :2]
     return np.hstack((R, t)), x1[inliers, :2], x2[inliers, :2]
 
 
 def main(argv):
 
     if len(argv) != 2:
-        print("usage: SIFT keypoint matching: <img1> <img2>")
+        print("usage: Motion reconstruction: <img1> <img2>")
         sys.exit(1)
 
     im1 = Image.open(argv[0])
@@ -115,48 +115,40 @@ def main(argv):
     im_2 = plt.imread(argv[1])
 
     sw = SIFTWrapper(im_1, im_2)
-    u1, u2 = sw.compute_best_matches(0.7)
-    # plot_matches(im_1, u1, im_2, u2)
+    u1, u2 = sw.compute_best_matches(0.9)
 
     K_cam = intrinsic_cam_mtx(f, cu, cv)
-    extrinsic_cam, u1_inliers, u2_inliers = get_inliers(u1, u2, K_cam)
+    extrinsic_cam, x1_inliers, x2_inliers = get_inliers(u1, u2, K_cam)
 
-    # P_0 = K_cam @ np.array([[1, 0, 0, 0],
-    #                         [0, 1, 0, 0],
-    #                         [0, 0, 1, 0]])
-    # P_1 = K_cam @ extrinsic_cam
-
+    # Note that we are using generalized
+    # camera coordinates, so we do not
+    # need to multiply by K
     P_0 = np.array([[1, 0, 0, 0],
                     [0, 1, 0, 0],
                     [0, 0, 1, 0]])
 
     P_1 = extrinsic_cam
 
-    c1 = Camera(f, sensor_size, P_0)
-    c2 = Camera(f, sensor_size, P_1)
-
-    cam_list = [c1, c2]
-
-    ic = InverseCamera()
-    X0 = calc_X0(cam_list) # + 0.2
-
     point_estimates = []
-    for u1_pt, u2_pt in zip(u1_inliers, u2_inliers):
+    for x1_pt, x2_pt in zip(x1_inliers, x2_inliers):
 
-        point_estimates.append(ic.estimate_points(cam_list, np.row_stack((u1_pt, u2_pt)).T, X0))
+        general_point = triangulate(P_0, P_1, x1_pt, x2_pt)
+        general_point /= general_point[3]
+
+        point_estimates.append(general_point[:3])
 
     # Create plot
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
-    # for point in point_estimates:
-    #
-    #     # print(point)
-    #     x, y, z = point
-    #     ax.scatter(x, y, z, alpha=0.8, edgecolors='none', s=30)
-    #
-    # plt.show()
-    #
+    for point in point_estimates:
+
+        # print(point)
+        x, y, z = point
+        ax.scatter(x, y, z, alpha=0.8, edgecolors='none', s=30)
+
+    plt.show()
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
